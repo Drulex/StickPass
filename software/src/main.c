d32c618ed2e580e78f5620d5c11f234dec6b8686
Alexandru Jora <alexandru@jora.ca>
2016-01-29 09:38:03 -0500

    Add some test code to flash LED via USB.

--- software/src/main.c
+++ software/src/main.c
@@ -1,14 +1,58 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+
+#include "usbdrv.h"
+
+#define LED_1 (1<<PB0)
+#define USB_LED_OFF 0
+#define USB_LED_ON  1
+
+
+// this gets called when custom control message is received
+USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
+    usbRequest_t *rq = (void *)data; // cast data to correct type
+
+    switch(rq->bRequest) { // custom command is in the bRequest field
+    case USB_LED_ON:
+        PORTB |= LED_1; // turn LED on
+        return 0;
+    case USB_LED_OFF:
+        PORTB &= ~LED_1; // turn LED off
+        return 0;
+    }
+
+    return 0; // should not get here
+}
 
 int main() {
-        DDRB |= 1; // LED on PB0
-        while(1) {
-                PORTB |= 1; // Turn LED on
-                _delay_ms(500);
-                PORTB &= ~1; // Turn LED off
-                _delay_ms(500);
-        }
+    uchar i;
+    DDRB = 1; // PB0 as output
 
-        return 1;
+    wdt_enable(WDTO_1S); // enable 1s watchdog timer
+
+    usbInit();
+
+    //turn on both LEDS
+    PORTB |= LED_1;
+
+    usbDeviceDisconnect(); // enforce re-enumeration
+    for(i = 0; i<250; i++) { // wait 500 ms
+        wdt_reset(); // keep the watchdog happy
+        _delay_ms(2);
+    }
+    // turn off LED1
+    PORTB &= ~LED_1;
+
+    usbDeviceConnect();
+
+    sei(); // Enable interrupts after re-enumeration
+
+    while(1) {
+        wdt_reset(); // keep the watchdog happy
+        usbPoll();
+    }
+
+    return 0;
 }
